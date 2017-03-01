79e38d8625c6a2cc560b45adcf287f87ef9cceac
supriya <supriya.malla@gmail.com>
2017-03-01 17:08:59 +0530

    delete/view/insert questions done.

--- LineParTest/WebContent/Admin/AdminQuestion/insertQuestion.jsp
+++ LineParTest/WebContent/Admin/AdminQuestion/insertQuestion.jsp
@@ -1,5 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@page import="java.util.ArrayList"%>
+
+    <%@page import="com.test.bean.Subject" %> 
+    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -12,9 +16,28 @@
 <body>
 <div class="form"> 
       <div class="tab-content">
-<h1>Inserted Form</h1>
+<h1>Please enter the question</h1>
+
 </div>
  <br>
+ <div>
+ <form action="${pageContext.request.contextPath}/QuestionController">
+  <% Subject sub=(Subject)session.getAttribute("sessionSubject"); %>
+  You requested to insert a question for the subject: <%=sub.getSubject() %>
+ <br>
+<input type="hidden" name="subject" value="0">
+ Subject ID<input type="text" name="subjectId" value="<%=sub.getSubjectId() %>" "fixed value" readonly >
+ Question<input type="text" name="question"><br>
+ Option 1 <input type="text" name="op1">
+ Option 2 <input type="text" name="op2">
+ Option 3 <input type="text" name="op3" >
+ Option 4 <input type="text" name="op4">
+ <br>
+ Answer <input type="number" name="answer">
+ </div>
+ <br>
+ <input type="submit" value="submit" name="insert">
+ </form>
  <form action="${pageContext.request.contextPath}/Admin/adminSignIn.jsp" method="post">
  <input type="submit" class="button-block" value="Back">
  </form>
--- LineParTest/src/com/test/controller/QuestionController.java
+++ LineParTest/src/com/test/controller/QuestionController.java
@@ -24,21 +24,66 @@ public class QuestionController extends HttpServlet {
 	private QuestionLogic qLogic=new QuestionLogic();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 System.out.println("ques");
+	 //System.out.println("ques");
 		HttpSession session=request.getSession(false);
 		int id=Integer.parseInt(request.getParameter("subject"));
 		session.setAttribute("sessionSubjectId", id);
+		
 		if (session.getAttribute("call").equals("insert")){
-			System.out.println("iasd");
-			
+			//System.out.println("iasd");
 			try {
 				sub=sLogic.search(id);
+				
 					session.setAttribute("sessionSubject",sub);//use this attribute to abstract info
-					response.sendRedirect("./Admin/AdminQuestion/insertQuestion.jsp");
+					session.setAttribute("call", "insertIntoTable");
+					 response.sendRedirect("./Admin/AdminQuestion/insertQuestion.jsp");
 			} catch (ClassNotFoundException | SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+		else if(session.getAttribute("call").equals("insertIntoTable")){
+			int subjectId=Integer.parseInt(request.getParameter("subjectId"));
+			String question=request.getParameter("question");
+			String op1=request.getParameter("op1");
+			String op2=request.getParameter("op2");
+			String op3=request.getParameter("op3");
+			String op4=request.getParameter("op4");
+			int answer=Integer.parseInt(request.getParameter("answer"));
+			Question q=new Question(id, subjectId, question, answer, op1, op2, op3, op4, op1);
+			try {
+				if(qLogic.insert(q)){
+					session.setAttribute("message","Inserted Successfully");
+						response.sendRedirect("./Admin/AdminQuestion/final.jsp");
+					}
+				else
+				{
+					session.setAttribute("message","error");
+					response.sendRedirect("./lost.jsp");
+				}
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else if(session.getAttribute("call").equals("deleteQues")){
+			int quesId=Integer.parseInt(request.getParameter("subject"));
+			try {
+				if(qLogic.delete(quesId)){
+					session.setAttribute("message","Deleted Successfully");
+						response.sendRedirect("./Admin/AdminQuestion/final.jsp");
+					}
+				else
+				{
+					session.setAttribute("message","error");
+					response.sendRedirect("./lost.jsp");
+				}
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 		else if (session.getAttribute("call").equals("search")) {
 			try {
@@ -99,24 +144,8 @@ public class QuestionController extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
-		else if(session.getAttribute("call").equals("deleteQues")){
-			int quesId=Integer.parseInt(request.getParameter("subject"));
-			try {
-				if(qLogic.delete(quesId)){
-					session.setAttribute("message","Deleted Successfully");
-						response.sendRedirect("./Admin/AdminQuestion/final.jsp");
-					}
-				else
-				{
-					session.setAttribute("message","error");
-					response.sendRedirect("./lost.jsp");
-				}
-			} catch (ClassNotFoundException | SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
+		
+		
 	}
  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
